9322ac497d1b8860de9c7d9c4656a0fef2825838
ljstaib <ljstaib@bu.edu>
2022-04-18 17:44:52 -0400

    Working on file hash checker

--- implant_crypto/check_filename_hash.cpp
+++ implant_crypto/check_filename_hash.cpp
@@ -1,12 +1,14 @@
 #include <windows.h>
 #include <bcrypt.h>
+#include <iostream>
+#include <shlwapi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <shlwapi.h>
-#include <iostream>
 
 #define FILENAME "C:\\malware\\ch0nky.txt"
+#define FILENAMELEN 22
 // Obtained with: (Get-FileHash C:\malware\ch0nky.txt).Hash.ToLower()
+// 25279a7b1e82c890e5a789f5d838a36a195c080c6565a7692f5ef40634a40aef
 #define FILEHASH "25279a7b1e82c890e5a789f5d838a36a195c080c6565a7692f5ef40634a40aef"
 
 BOOL checkForFile() {
@@ -23,58 +25,105 @@ BOOL checkForFileHash() {
 	NTSTATUS status;
 	BCRYPT_ALG_HANDLE algHandle;
 
-	if (BCryptOpenAlgorithmProvider(
-			algHandle,
+	if (!BCRYPT_SUCCESS(BCryptOpenAlgorithmProvider(
+			&algHandle,
 			BCRYPT_SHA256_ALGORITHM,
 			NULL,
-			0) == STATUS_SUCCESS) {
+			0))) {
 		std::cout << "[ERROR] Could not initialize hashing handle." << std::endl;
     	return false;
 	}
 
 	// 2. Retrieve value of length for hashing object
-	PUCHAR hashLen;
-	if (BCryptGetProperty(
+	DWORD hashLen;
+	ULONG result;
+	if (!BCRYPT_SUCCESS(BCryptGetProperty(
 			algHandle,
 			BCRYPT_OBJECT_LENGTH,
-			hashLen,
+			(PUCHAR)&hashLen,
 			sizeof(hashLen),
-			NULL,
-			0) == STATUS_SUCCESS) {
+			&result,
+			0))) {
 		std::cout << "[ERROR] Could not retrieve length for hashing object." << std::endl;
-    	return false;
+        BCryptCloseAlgorithmProvider(algHandle, 0);
+        return false;
 	}
 
 	// 3. Allocate memory and create hash
 	BCRYPT_HASH_HANDLE hashHandle;
-	PUCHAR hashObj = malloc(hashLen);
+	PUCHAR hashObj = (PUCHAR)malloc(hashLen);
 
-	if (BCryptCreateHash(
+	if (!BCRYPT_SUCCESS(BCryptCreateHash(
 			algHandle,
 			&hashHandle,
 			hashObj,
-			(ULONG) hashLen,
+			hashLen,
 			NULL,
 			0,
-			0) == STATUS_SUCCESS) {
+			0))) {
 		std::cout << "[ERROR] Could not create hashing object." << std::endl;
+        BCryptCloseAlgorithmProvider(algHandle, 0);
+        BCryptDestroyHash(hashHandle);
+        free(hashObj);
     	return false;
 	}
 
-	// 4. Retrieve hash
-	// if (BCryptFinishHash(
-	// 		&hashHandle,
-	// 		hashObj,
-	// 		(ULONG) hashLen,
-	// 		NULL,
-	// 		0,
-	// 		0) 
-	// 	!= STATUS_SUCCESS) {
-	// 	std::cout << "[ERROR] Could not create hashing object." << std::endl;
- //    	return false;
-	// }
+	// 4. Create hash
+	if (!BCRYPT_SUCCESS(BCryptHashData(
+			hashHandle,
+			(PUCHAR) FILENAME,
+			FILENAMELEN,
+			0))) {
+		std::cout << "[ERROR] Could not hash file." << std::endl;
+        BCryptCloseAlgorithmProvider(algHandle, 0);
+        BCryptDestroyHash(hashHandle);
+        free(hashObj);
+    	return false;
+	}
 
-	printf("%s\n", hashObj);
+	// 5. Retrieve hash, length is 32 bytes or 64 hex chars
+	PUCHAR fileHash = (PUCHAR)malloc(32);
+	if (!BCRYPT_SUCCESS(BCryptFinishHash(
+			hashHandle,
+			fileHash,
+			32,
+			0))) {
+		std::cout << "[ERROR] Could not retrieve file hash." << std::endl;
+        BCryptCloseAlgorithmProvider(algHandle, 0);
+        BCryptDestroyHash(hashHandle);
+        free(hashObj);
+        free(fileHash);
+    	return false;
+	}
+
+	// 6. Compare hash with desired hash
+
+	printf("%s\n", fileHash);
+	printf("%s\n", FILEHASH);
+
+	BOOL out;
+	if (fileHashHex == FILEHASH) {
+		out = true;
+	}
+	else {
+		out = false;
+	}
+
+	// 7. Cleanup
+    if (algHandle) {
+        BCryptCloseAlgorithmProvider(algHandle, 0);
+    }
+	if (hashHandle) {
+        BCryptDestroyHash(hashHandle);
+    }
+	if (hashObj) {
+        free(hashObj);
+    }
+	if (fileHash) {
+        free(fileHash);
+    }
+
+    return out;
 }
 
 int main(int argc, char* argv[]) {
@@ -91,13 +140,14 @@ int main(int argc, char* argv[]) {
 		printf("ch0nky.txt hash found!\n");
 	}
 	else {
-		printf("ch0nky.txt hash found...\n");
+		printf("ch0nky.txt hash not found...\n");
 	}
 
-	if (checkForFile() && checkForFileHash()) {
-		printf("ch0nky.txt file exists and is verified.");
-	}
-	else {
-		printf("ch0nky.txt file does not exist and/or is not verified.\n");
-	}
+	// if (checkForFile() && checkForFileHash()) {
+	// 	printf("ch0nky.txt file exists and is verified.");
+	// }
+	// else {
+	// 	printf("ch0nky.txt file does not exist and/or is not verified.\n");
+	// }
+	return 0;
 }
