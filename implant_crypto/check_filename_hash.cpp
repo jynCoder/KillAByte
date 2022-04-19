4fcee4b3f6e069b232df2fd3007a978901663579
ljstaib <ljstaib@bu.edu>
2022-04-19 17:22:55 -0400

    Fixed filename and filename hash checkers

--- implant_crypto/check_filename_hash.cpp
+++ implant_crypto/check_filename_hash.cpp
@@ -1,15 +1,13 @@
 #include <windows.h>
 #include <bcrypt.h>
+#include <wincrypt.h>
 #include <iostream>
 #include <shlwapi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define FILENAME "C:\\malware\\ch0nky.txt"
-#define FILENAMELEN 22
-// Obtained with: (Get-FileHash C:\malware\ch0nky.txt).Hash.ToLower()
-// 25279a7b1e82c890e5a789f5d838a36a195c080c6565a7692f5ef40634a40aef
-#define FILEHASH "25279a7b1e82c890e5a789f5d838a36a195c080c6565a7692f5ef40634a40aef"
+#define FILENAMELEN 21
 
 BOOL checkForFile() {
 	if (PathFileExistsA(FILENAME)) {
@@ -96,13 +94,37 @@ BOOL checkForFileHash() {
     	return false;
 	}
 
+	// 6. Convert to hex
+	DWORD confirmSize;
+
+    BOOL bufsizeCheck = CryptBinaryToStringA(
+    	fileHash,
+    	_msize(fileHash),
+    	CRYPT_STRING_HEX,
+    	NULL,
+    	&confirmSize
+    );
+
+    if (bufsizeCheck == 0) {
+    	std::cout << "[ERROR] Obtaining the correct size for the hex buffer failed." << std::endl;
+    	return false;
+    }
+
+    LPSTR buf = (LPSTR) malloc(confirmSize * sizeof(LPSTR));
+
+    BOOL bufCheck = CryptBinaryToStringA(
+    	fileHash,
+    	_msize(fileHash),
+    	CRYPT_STRING_HEX | CRYPT_STRING_NOCRLF,
+    	buf,
+    	&confirmSize
+    );
+
 	// 6. Compare hash with desired hash
+	std::string FILENAMEHASH = "f8 54 ae 18 4b f1 7d 97 c3 ae 33 91 a8 d4 94 96 5d 43 1b b4 65 05 08 09 9c 6e ce 4c 8c 40 43 43";
 
-	printf("%s\n", fileHash);
-	printf("%s\n", FILEHASH);
-
-	BOOL out;
-	if (fileHashHex == FILEHASH) {
+	BOOL out = false;
+	if (FILENAMEHASH.compare(buf) == 0) {
 		out = true;
 	}
 	else {
@@ -130,24 +152,16 @@ int main(int argc, char* argv[]) {
 	printf("Checking for ch0nky.txt!\n");
 	if (checkForFile()) {
 		printf("ch0nky.txt file found!\n");
+		printf("Checking for hash of ch0nky.txt!\n");
+		if (checkForFileHash()) {
+			printf("ch0nky.txt hash found!\n");
+		}
+		else {
+			printf("ch0nky.txt hash not found...\n");
+		}
 	}
 	else {
 		printf("ch0nky.txt file not found...\n");
 	}
-
-	printf("Checking for hash of ch0nky.txt!\n");
-	if (checkForFileHash()) {
-		printf("ch0nky.txt hash found!\n");
-	}
-	else {
-		printf("ch0nky.txt hash not found...\n");
-	}
-
-	// if (checkForFile() && checkForFileHash()) {
-	// 	printf("ch0nky.txt file exists and is verified.");
-	// }
-	// else {
-	// 	printf("ch0nky.txt file does not exist and/or is not verified.\n");
-	// }
 	return 0;
 }
