84773e041cca0c993c4db0376a2f0830afb41588
ljstaib <ljstaib@bu.edu>
2022-04-28 20:03:41 -0400

    Started loot

---
+++ implant_crypto/asymmetric_crypto.cpp
@@ -0,0 +1,200 @@
+#include <windows.h>
+#include <bcrypt.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NT_SUCCESS(Status)          (((NTSTATUS)(Status)) >= 0)
+#define STATUS_UNSUCCESSFUL         ((NTSTATUS)0xC0000001L)
+
+BCRYPT_ALG_HANDLE algHandle;
+BCRYPT_KEY_HANDLE keyHandle;
+
+void cleanUp() {
+	if (algHandle) {
+		BCryptCloseAlgorithmProvider(algHandle, 0);
+		algHandle = NULL;
+	}
+	if (keyHandle) {
+		BCryptDestroyKey(keyHandle);
+		keyHandle = NULL;
+	}
+}
+
+BCRYPT_KEY_HANDLE generateKeyRSA() {
+	// 1. Initalize a CNG (cryptography next-genertion) provider for RSA
+	NTSTATUS status;
+
+	if (!BCRYPT_SUCCESS(BCryptOpenAlgorithmProvider(
+			&algHandle,
+			BCRYPT_RSA_ALGORITHM,
+			NULL,
+			0))) {
+		std::cout << "[ERROR] Could not initialize RSA handle." << std::endl;
+		cleanUp();
+    	return NULL;
+	}
+
+	// Can also generate keys with BCryptImportKey or BCryptImportKeyPair
+	// We will probably use this instead. If this is the case, I can
+	// make a new function.
+
+	// 2. Generate key pair for RSA
+
+	if (!BCRYPT_SUCCESS(BCryptGenerateKeyPair(
+			algHandle,
+			&keyHandle,
+			1024, //Key length is 1024, can be adjusted here
+			0))) {
+		std::cout << "[ERROR] Key pair for RSA could not be generated." << std::endl;
+		cleanUp();
+		return NULL;
+	}
+
+
+	// 2.5. Set key properties? Not sure if necessary.
+
+	// 3. Finalize key pair 
+	if (!BCRYPT_SUCCESS(BCryptFinalizeKeyPair(
+			keyHandle,
+			0))) {
+		std::cout << "[ERROR] Key pair for RSA could not be finalized." << std::endl;
+		cleanUp();
+		return NULL;
+	}
+
+	return keyHandle;
+}
+
+char* encryptRSA(BCRYPT_KEY_HANDLE keyHandle, char* plainText) {
+	//Args:
+	//	keyHandle - key handle generated in generateKeyRSA()
+	//	plainText - text to encrypt (string for now)
+
+	//1. Obtain size required for ciphertext
+    ULONG confirmSize;
+
+	if (!BCRYPT_SUCCESS(BCryptEncrypt(
+			keyHandle,
+			(PUCHAR) plainText,
+			(ULONG) strlen(plainText),
+			NULL,
+			NULL,
+			0,
+			NULL,
+			0,
+			&confirmSize,
+			BCRYPT_PAD_PKCS1))) {
+		std::cout << "[ERROR] RSA encryption failed (obtaining size for encrypted text)." << std::endl;
+		cleanUp();
+		return NULL;
+	}
+
+	// printf("confirmSize: ");
+    // printf("%lu\n", confirmSize);
+
+	// Create buffer with retrieved size
+    char* encryptedText = (char*) malloc(confirmSize + 1);
+    ULONG confirmData;
+
+    // 2. With size, can encrypt
+    NTSTATUS result = BCryptEncrypt(
+			keyHandle,
+			(PUCHAR) plainText,
+			(ULONG) strlen(plainText),
+			NULL,
+			NULL,
+			0,
+			(PUCHAR) encryptedText,
+			confirmSize,
+			&confirmData,
+			BCRYPT_PAD_PKCS1);
+
+    if (!BCRYPT_SUCCESS(result)) {
+		std::cout << "[ERROR] RSA encryption failed (generating encrypted text)." << std::endl;
+		cleanUp();
+		return NULL;
+	}
+
+	encryptedText[confirmSize] = '\0';
+	// printf("%s\n", encryptedText);
+
+    return encryptedText;
+}
+
+//TODO
+char* decryptRSA(BCRYPT_KEY_HANDLE keyHandle, char* encryptedText) {
+	//Args:
+	//	keyHandle - key handle generated in generateKeyRSA()
+	//	encryptedText - text to decrypt (char* for now)
+
+	// 1. Obtain size required for plaintext
+	ULONG confirmSize;
+
+	NTSTATUS result = BCryptDecrypt(
+			keyHandle,
+			(PUCHAR) encryptedText,
+			(ULONG) strlen(encryptedText),
+			NULL,
+			NULL,
+			0,
+			NULL,
+			0,
+			&confirmSize,
+			BCRYPT_PAD_PKCS1);
+
+	if (!BCRYPT_SUCCESS(result)) {
+		std::cout << "[ERROR] RSA decryption failed (obtaining size for decrypted text)." << std::endl;
+		cleanUp();
+		return NULL;
+	}
+
+    char* plainText = (char*) malloc(confirmSize + 1);
+    ULONG confirmData;
+
+    if (!BCRYPT_SUCCESS(BCryptDecrypt(
+			keyHandle,
+			(PUCHAR) encryptedText,
+			(ULONG) strlen(encryptedText),
+			NULL,
+			NULL,
+			0,
+			(PUCHAR) plainText,
+			confirmSize,
+			&confirmData,
+			BCRYPT_PAD_PKCS1))) {
+		std::cout << "[ERROR] RSA decryption failed (obtaining size for decrypted text)." << std::endl;
+		cleanUp();
+		return NULL;
+	}
+
+	plainText[confirmSize] = '\0';
+
+    return plainText;
+}
+
+int main(int argc, char* argv[]) {
+	printf("[INFO] Testing RSA!\n");
+	BCRYPT_KEY_HANDLE keyHandle = generateKeyRSA();
+	if (keyHandle) {
+		printf("[INFO] Key pair successfully generated!\n");
+	}
+
+	char plaintext_test[] = "CS501: Introduction to Malware, Threat Hunting and Offensive Capabilities Development";
+	char* encryptedText = encryptRSA(keyHandle, plaintext_test);
+
+	if (encryptedText) {
+		printf("[INFO] Plaintext successfully encrypted!\n");
+	}
+
+	char* decryptedText = decryptRSA(keyHandle, encryptedText);
+
+	if (strlen(decryptedText) != 0) {
+		printf("[INFO] Encrypted text successfully decrypted!\n");
+		printf("%s\n", decryptedText);
+	}
+
+	cleanUp();
+	return 0;
+}
---
+++ implant_execution/createProcess.c
@@ -0,0 +1,204 @@
+#include <windows.h>
+#include <stdio.h>
+
+#define BUF_SIZE 4096
+
+int main(int argc, char* argv[]){
+
+    if (argc != 3){
+        printf("Usage: %s program.exe \"args and args \"", argv[0]);
+        return 0;
+    }
+
+    // parse args
+    char* program = argv[1];
+    char* args = argv[2];
+
+    // create buffer for cmdline argument
+    // copied from previous Q, removing redirect operator
+    char* buf = NULL;
+    int buf_len = strlen("c:\\windows\\system32\\cmd.exe /c  ") + strlen(program) + strlen(args);
+    buf = (char*) malloc(buf_len + 1);
+
+    // Make sure that the program was able to allocate memory with malloc
+    if (buf == NULL) {
+        printf("[ERROR] Unable to allocate memory.");
+        return 0;
+    }
+
+    int check = sprintf(buf, "c:\\windows\\system32\\cmd.exe /c %s %s", program, args);
+
+    if (check < 0) {
+        printf("[ERROR] Unable to create command line.");
+        return 0;
+    }
+
+    // Declare handles for StdOut
+    HANDLE hStdOutRead, hStdOutWrite;
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+    // Prevent dead squirrels 
+    ZeroMemory(&pi, sizeof(pi));
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    // TODO: Set si.dwFlags...
+    // HINT Read this and look for anything that talks about handle inheritance :-)
+    //  https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/ns-processthreadsapi-startupinfoa
+    
+    si.dwFlags |= STARTF_USESTDHANDLES;
+
+    SECURITY_ATTRIBUTES sa;
+    sa.nLength = sizeof(sa);
+    sa.lpSecurityDescriptor = NULL;
+    sa.bInheritHandle = TRUE;
+
+    // Create process
+    // TODO: ensure that the child processes can inherit our handles!
+
+    // Create parent process, 5th arg set to true, child process will inherit handles
+    BOOL processTest = CreateProcessA(
+            NULL,
+            buf,
+            &sa,
+            &sa,
+            TRUE,
+            NORMAL_PRIORITY_CLASS,
+            NULL,
+            NULL,
+            &si,
+            &pi
+        );
+
+    // Make sure parent process was created
+    if (processTest == 0) {
+        printf("[ERROR] The parent process could not be created.");
+        return 0;
+    }
+
+    // TODO: Create a pipe object and share the handle with a child process
+    // Use security attributes from parent process pipe, makes sure handle is inheritable
+    // SECURITY_ATTRIBUTES sa_pipe;
+    // sa_pipe.nLength = sizeof(sa_pipe);
+    // sa_pipe.lpSecurityDescriptor = NULL;
+    // sa_pipe.bInheritHandle = TRUE;
+
+    // Create a pipe, use BUF_SIZE parameter for buffer size
+    BOOL pipeTest = CreatePipe(
+            &hStdOutRead,
+            &hStdOutWrite,
+            &sa,
+            BUF_SIZE
+        );
+
+    // Make sure pipe was created
+    if (pipeTest == 0) {
+        printf("[ERROR] The pipe could not be created.");
+        return 0;
+    }
+
+    //Make sure read end is not inherited
+    BOOL handleInfoCheck = SetHandleInformation(hStdOutRead, HANDLE_FLAG_INHERIT, 0);
+
+    //Make sure changes were implemented
+    if (handleInfoCheck == 0) {
+        printf("[ERROR] Lack of inheritance for read end of the pipe could not be ensured");
+        return 0;
+    }
+
+    // TODO: Set
+    // set startupinfo handles
+    si.hStdInput = NULL;
+    si.hStdError = hStdOutWrite;
+    si.hStdOutput = hStdOutWrite;
+    si.dwFlags |= STARTF_USESTDHANDLES;
+
+    // Create the child Processes and wait for it to terminate!
+    BOOL processChildTest = CreateProcessA(
+            NULL,
+            buf,
+            NULL,
+            NULL,
+            TRUE,
+            NORMAL_PRIORITY_CLASS,
+            NULL,
+            NULL,
+            &si,
+            &pi
+        );
+
+    // Make sure child process was created
+    if (processChildTest == 0) {
+        printf("[ERROR] A child process could not be created.");
+        return 0;
+    }
+
+    // Create buffer to use to read from pipe
+    // Not adding one since reading 4095 bytes at a time, need 1 byte for null terminator
+    char* pipeBuf = NULL;
+    pipeBuf = (char*) malloc(BUF_SIZE);
+    DWORD bytesAvailable = 0;
+    DWORD bytesRead = 0;
+    DWORD bytesLeft = 0;
+
+    //Close write handle to pipe before reading
+    BOOL closeCheckPipeWrite = CloseHandle(hStdOutWrite);
+
+    if (closeCheckPipeWrite == 0) {
+        printf("[ERROR] Pipe write handle could not be closed.");
+        return 0;
+    }
+
+    // Read pipe
+    // While my child processes are still alive...
+    BOOL breakLoop = FALSE;
+    while (WaitForSingleObject(pi.hProcess, 0) == WAIT_TIMEOUT && breakLoop == FALSE) {
+        // While there is data to read from the pipe...
+        while (PeekNamedPipe(hStdOutRead, NULL, 0, NULL, &bytesAvailable, &bytesLeft) && breakLoop == FALSE) {
+            // Read data from pipe
+            BOOL readTest = ReadFile(
+                    hStdOutRead, 
+                    pipeBuf, 
+                    BUF_SIZE, 
+                    &bytesRead, 
+                    NULL);
+            // Check if pipe is readable
+            if (readTest == 0) {
+                // Ignore... messes with autograder
+                // printf("[ERROR] Pipe could not be read.");
+            }
+            else if (bytesRead == 0) {
+                // Ignore... messes with autograder
+                // printf("[ERROR] 0 bytes read from pipe.");
+            }
+            else {
+                printf("%s", pipeBuf);
+            }
+        }
+    }
+
+    // TODO: perform any cleanup necessary! 
+    // The parent processes no longer needs a handle to the child processes, the running thread, or the out file!
+    // //your solution here!
+    // Finally, print the contents from the pipe!
+    
+    BOOL closeCheck = CloseHandle(pi.hProcess);
+
+    if (closeCheck == 0) {
+        printf("[ERROR] Process handle could not be closed.");
+        return 0;
+    }
+
+    BOOL closeCheckPipeRead = CloseHandle(hStdOutRead);
+
+    if (closeCheckPipeRead == 0) {
+        printf("[ERROR] Pipe read handle could not be closed.");
+        return 0;
+    }
+
+    free(program);
+    free(args);
+    free(buf);
+    free(pipeBuf);
+
+    return 0;
+}
